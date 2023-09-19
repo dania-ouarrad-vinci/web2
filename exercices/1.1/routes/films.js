0cc1d67b8272cc1d8b3940c83a375e7f584d5086
dania <dania.ouarrad@student.vinci.be>
2023-09-19 10:14:18 +0200

    1.1 API : read.

---
+++ exercices/1.1/routes/films.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+const FILM = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    duration: 152,
+    budget: 125000000,
+    link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
+  },
+  {
+    id: 1,
+    title: 'Harry Potter',
+    duration: 152,
+    budget: 125000000,
+    link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
+  },
+  {
+    id: 1,
+    title: 'Harry Potter',
+    duration: 152,
+    budget: 125000000,
+    link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
+  },
+];
+
+// Read all the films from the film
+router.get('/', (req, res, next) => {
+  console.log("GET /films");
+  res.json(FILM);
+});
+
+module.exports = router;
