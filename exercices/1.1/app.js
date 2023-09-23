2d1451c294ca1797030c33df236782b7a782cb86
dania <dania.ouarrad@student.vinci.be>
2023-09-23 12:08:01 +0200

    1.1 API : read

--- exercices/1.1/app.js
+++ exercices/1.1/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var filmRouter = require('./routes/films');
-var usersRouter = require('./routes/users');
+var filmsRouter = require('./routes/films');
 
 var app = express();
 
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //renvoi index.html
 
-app.use('/films', filmRouter);
-app.use('/users', usersRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
--- exercices/1.1/routes/films.js
+++ exercices/1.1/routes/films.js
@@ -1,34 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-const FILM = [
+const films = [
   {
     id: 1,
     title: 'Harry Potter',
     duration: 152,
-    budget: 125000000,
+    budget: '125000000',
     link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
   },
   {
     id: 1,
     title: 'Harry Potter',
     duration: 152,
-    budget: 125000000,
+    budget: '125000000',
     link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
   },
   {
     id: 1,
     title: 'Harry Potter',
     duration: 152,
-    budget: 125000000,
+    budget: '125000000',
     link: "https://www.rottentomatoes.com/m/harry_potter_and_the_sorcerers_stone"
   },
 ];
 
-// Read all the films from the film
-router.get('/', (req, res, next) => {
+// Read all the films from the films
+router.get('/', function (req, res) {
   console.log("GET /films");
-  res.json(FILM);
+  res.json(films);
 });
 
 module.exports = router;
