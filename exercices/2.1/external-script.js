0655e0715330c367c7e62f676d25975e5ccc6b34
dania <dania.ouarrad@student.vinci.be>
2023-10-09 14:01:21 +0200

    2.1 : HMI : first external script

--- exercices/2.1/external-script.js
+++ exercices/2.1/external-script.js
@@ -1,7 +1,10 @@
 function addDateTime(message){
+    console.log(message);
     const dateTimNow = new Date;
     const result = dateTimNow.toLocaleDateString() + " " + dateTimNow.toLocaleTimeString() + " : " + message;
+    console.log(result);
     return result;
 }
 
-alert(addDateTime("his is the best moment to have a look at this website !"));
+alert(addDateTime("This is the best moment to have a look at this website !"));
+
